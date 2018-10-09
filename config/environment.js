6f6869ec1c3d7e8a39783bc060841a5535049ba4
Jozue06 <jozue06@gmail.com>
2018-10-09 16:50:39 -0700

    removes trvsyml

--- config/environment.js
+++ config/environment.js
@@ -23,25 +23,25 @@ module.exports = function(environment) {
     }
   };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+  // if (environment === 'development') {
+  //   // ENV.APP.LOG_RESOLVER = true;
+  //   // ENV.APP.LOG_ACTIVE_GENERATION = true;
+  //   // ENV.APP.LOG_TRANSITIONS = true;
+  //   // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+  //   // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  // }
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
+  // if (environment === 'test') {
+  //   // Testem prefers this...
+  //   ENV.locationType = 'none';
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+  //   // keep test console output quieter
+  //   ENV.APP.LOG_ACTIVE_GENERATION = false;
+  //   ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
-    ENV.APP.autoboot = false;
-  }
+  //   ENV.APP.rootElement = '#ember-testing';
+  //   ENV.APP.autoboot = false;
+  // }
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
